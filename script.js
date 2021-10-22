8b41a266d37b745589ec196497548619748539a4
Southern Comfort <scottguenther94@gmail.com>
2021-10-22 18:35:35 -0400

    Revamped Page

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+  console.log("hey baby");
+  
